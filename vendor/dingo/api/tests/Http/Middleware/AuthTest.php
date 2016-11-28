b0ff73914abfb30cba2df3a1ce32baa00548ee1f
q1082121 <rubbish.boy@163.com>
2016-11-28 09:35:29 +0800

    API扩展包文件补充

---
+++ vendor/dingo/api/tests/Http/Middleware/AuthTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Dingo\Api\Tests\Http\Middleware;
+
+use Mockery as m;
+use Dingo\Api\Auth\Auth;
+use Dingo\Api\Http\Request;
+use Dingo\Api\Routing\Route;
+use Dingo\Api\Routing\Router;
+use PHPUnit_Framework_TestCase;
+use Illuminate\Container\Container;
+use Dingo\Api\Http\Middleware\Auth as AuthMiddleware;
+use Dingo\Api\Tests\Stubs\RoutingAdapterStub;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class AuthTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->container = new Container;
+        $this->adapter = new RoutingAdapterStub;
+        $this->router = m::mock(Router::class);
+        $this->auth = m::mock(Auth::class);
+        $this->middleware = new AuthMiddleware($this->router, $this->auth);
+    }
+
+    public function testProtectedRouteFiresAuthenticationAndPasses()
+    {
+        $request = Request::create('test', 'GET');
+
+        $route = new Route($this->adapter, $this->container, $request, new \Illuminate\Routing\Route('GET', '/test', [
+            'providers' => [],
+        ]));
+
+        $this->auth->shouldReceive('check')->once()->with(false)->andReturn(false);
+        $this->auth->shouldReceive('authenticate')->once()->with([])->andReturn(null);
+
+        $this->router->shouldReceive('getCurrentRoute')->once()->andReturn($route);
+
+        $this->middleware->handle($request, function ($handledRequest) use ($request) {
+            $this->assertSame($handledRequest, $request);
+        });
+    }
+
+    public function testProtectedRouteAlreadyLoggedIn()
+    {
+        $request = Request::create('test', 'GET');
+
+        $route = new Route($this->adapter, $this->container, $request, new \Illuminate\Routing\Route('GET', '/test', [
+            'providers' => [],
+        ]));
+
+        $this->auth->shouldReceive('check')->once()->with(false)->andReturn(true);
+
+        $this->router->shouldReceive('getCurrentRoute')->once()->andReturn($route);
+
+        $this->middleware->handle($request, function ($handledRequest) use ($request) {
+            $this->assertSame($handledRequest, $request);
+        });
+    }
+
+    /**
+     * @expectedException \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
+     */
+    public function testAuthenticationFailsAndExceptionIsThrown()
+    {
+        $exception = new UnauthorizedHttpException('test');
+
+        $request = Request::create('test', 'GET');
+
+        $route = new Route($this->adapter, $this->container, $request, new \Illuminate\Routing\Route('GET', '/test', [
+            'providers' => [],
+        ]));
+
+        $this->auth->shouldReceive('check')->once()->with(false)->andReturn(false);
+        $this->auth->shouldReceive('authenticate')->once()->with([])->andThrow($exception);
+
+        $this->router->shouldReceive('getCurrentRoute')->once()->andReturn($route);
+
+        $this->middleware->handle($request, function () {
+            //
+        });
+    }
+}

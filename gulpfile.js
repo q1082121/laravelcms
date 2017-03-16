4fa62ff68ab362931912311db0dee4bced4ac1ea
q1082121 <rubbish.boy@163.com>
2017-03-16 10:21:48 +0800

    gulp 管理less css资源

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var elixir = require('laravel-elixir');
-
+var gulp = require('gulp');
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -10,7 +10,26 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
+var rootSrc="resources/assets/";
+var rootCssSrc=rootSrc+"css/";
+var publicSrc="public/";
+var publicCssSrc="public/css/";
 var adminSrc="admin/";
+var homeSrc="home/";
+var userSrc="user/";
+var rootAdminCssSrc=rootCssSrc+adminSrc;
+var rootHomeCssSrc=rootCssSrc+homeSrc;
+var rootUserCssSrc=rootCssSrc+userSrc;
+var publicAdminCssSrc=publicCssSrc+adminSrc;
+var publicHomeCssSrc=publicCssSrc+homeSrc;
+var publicUserCssSrc=publicCssSrc+userSrc;
 elixir(function (mix) {
-    mix.less(adminSrc+'*.less','public/css/'+adminSrc+'admin.css')
+    //编译less至css目录
+    mix.less(adminSrc+'admin.less',rootAdminCssSrc+'admin.css')
+    mix.less(homeSrc+'home.less',rootHomeCssSrc+'home.css')
+    mix.less(userSrc+'user.less',rootUserCssSrc+'user.css')
+    //css目录里各目录css至public/css下各目录
+    mix.stylesIn(rootAdminCssSrc,publicAdminCssSrc+'admin.css')
+    mix.stylesIn(rootHomeCssSrc,publicHomeCssSrc+'home.css')
+    mix.stylesIn(rootUserCssSrc,publicUserCssSrc+'user.css')
 });

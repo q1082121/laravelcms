c9c39d30a5009bba88905f9fc92fe216ed4dfe68
q1082121 <rubbish.boy@163.com>
2017-03-16 13:05:10 +0800

    修改路径

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -33,7 +33,7 @@
   <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/daterangepicker/daterangepicker.css')}}">
   <!-- bootstrap wysihtml5 - text editor -->
   <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
-  <link rel="stylesheet" href="{{asset('/css/admin/admin.css')}}">
+  <link rel="stylesheet" href="{{ elixir('css/admin.css') }}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->

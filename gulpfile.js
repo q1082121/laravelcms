2b119cadadb3e0aeb7d1789ff0950aed2506ba69
q1082121 <rubbish.boy@163.com>
2017-03-16 10:24:23 +0800

    格式化代码

--- gulpfile.js
+++ gulpfile.js
@@ -10,26 +10,26 @@ var gulp = require('gulp');
  | file for our application, as well as publishing vendor resources.
  |
  */
-var rootSrc="resources/assets/";
-var rootCssSrc=rootSrc+"css/";
-var publicSrc="public/";
-var publicCssSrc="public/css/";
-var adminSrc="admin/";
-var homeSrc="home/";
-var userSrc="user/";
-var rootAdminCssSrc=rootCssSrc+adminSrc;
-var rootHomeCssSrc=rootCssSrc+homeSrc;
-var rootUserCssSrc=rootCssSrc+userSrc;
-var publicAdminCssSrc=publicCssSrc+adminSrc;
-var publicHomeCssSrc=publicCssSrc+homeSrc;
-var publicUserCssSrc=publicCssSrc+userSrc;
+var rootSrc = "resources/assets/";
+var rootCssSrc = rootSrc + "css/";
+var publicSrc = "public/";
+var publicCssSrc = "public/css/";
+var adminSrc = "admin/";
+var homeSrc = "home/";
+var userSrc = "user/";
+var rootAdminCssSrc = rootCssSrc + adminSrc;
+var rootHomeCssSrc = rootCssSrc + homeSrc;
+var rootUserCssSrc = rootCssSrc + userSrc;
+var publicAdminCssSrc = publicCssSrc + adminSrc;
+var publicHomeCssSrc = publicCssSrc + homeSrc;
+var publicUserCssSrc = publicCssSrc + userSrc;
 elixir(function (mix) {
     //编译less至css目录
-    mix.less(adminSrc+'admin.less',rootAdminCssSrc+'admin.css')
-    mix.less(homeSrc+'home.less',rootHomeCssSrc+'home.css')
-    mix.less(userSrc+'user.less',rootUserCssSrc+'user.css')
+    mix.less(adminSrc + 'admin.less', rootAdminCssSrc + 'admin.css')
+    mix.less(homeSrc + 'home.less', rootHomeCssSrc + 'home.css')
+    mix.less(userSrc + 'user.less', rootUserCssSrc + 'user.css')
     //css目录里各目录css至public/css下各目录
-    mix.stylesIn(rootAdminCssSrc,publicAdminCssSrc+'admin.css')
-    mix.stylesIn(rootHomeCssSrc,publicHomeCssSrc+'home.css')
-    mix.stylesIn(rootUserCssSrc,publicUserCssSrc+'user.css')
+    mix.stylesIn(rootAdminCssSrc, publicAdminCssSrc + 'admin.css')
+    mix.stylesIn(rootHomeCssSrc, publicHomeCssSrc + 'home.css')
+    mix.stylesIn(rootUserCssSrc, publicUserCssSrc + 'user.css')
 });

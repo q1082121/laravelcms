3013363f027f84ff1cd8a9095761f0323214ba06
q1082121 <rubbish.boy@163.com>
2017-03-23 10:38:34 +0800

    合并引入AdminLTE资源

--- resources/views/layouts/admin_iframe.blade.php
+++ resources/views/layouts/admin_iframe.blade.php
@@ -16,25 +16,13 @@
   <link rel="stylesheet" href="{{asset('/module/AdminLTE/dist/css/font-awesome/4.6.3/css/font-awesome.min.css')}}" >
   <!-- Ionicons -->
   <link rel="stylesheet" href="{{asset('/module/AdminLTE/dist/css/ionicons/2.0.1/css/ionicons.min.css')}}" >
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('/module/AdminLTE/dist/css/AdminLTE.min.css')}}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="{{asset('/module/AdminLTE/dist/css/skins/_all-skins.min.css')}}">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/iCheck/all.css')}}">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/morris/morris.css')}}">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/datepicker/datepicker3.css')}}">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/daterangepicker/daterangepicker.css')}}">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="{{asset('/module/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
-  <link rel="stylesheet" href="{{ elixir('css/admin.css') }}">
-
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -48,7 +36,6 @@
   <!--common-->
   <script src="{{ elixir('js/admin.js') }}"></script>
 
-  
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
@@ -68,45 +55,21 @@
 </div>
 <!-- ./wrapper -->
 
-<!-- jQuery UI 1.11.4 -->
-<script src="{{asset('/module/AdminLTE/dist/js/jquery-ui/1.11.4/jquery-ui.min.js')}}"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
 <!-- Bootstrap 3.3.6 -->
 <script src="{{asset('/module/AdminLTE/bootstrap/js/bootstrap.min.js')}}"></script>
-<!-- Morris.js charts 
-<script src="{{asset('/module/AdminLTE/dist/js/raphael/2.1.0/raphael-min.js')}}"></script>
-<script src="{{asset('/module/AdminLTE/plugins/morris/morris.min.js')}}"></script>
--->
 <!-- Sparkline -->
 <script src="{{asset('/module/AdminLTE/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
 <!-- jvectormap -->
 <script src="{{asset('/module/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
 <script src="{{asset('/module/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{asset('/module/AdminLTE/plugins/knob/jquery.knob.js')}}"></script>
-<!-- daterangepicker -->
-<script src="{{asset('/module/AdminLTE/dist/js/moment/2.11.2/min/moment.min.js')}}"></script>
-<script src="{{asset('/module/AdminLTE/plugins/daterangepicker/daterangepicker.js')}}"></script>
-<!-- datepicker -->
-<script src="{{asset('/module/AdminLTE/plugins/datepicker/bootstrap-datepicker.js')}}"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="{{asset('/module/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
 <!-- Slimscroll -->
 <script src="{{asset('/module/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
 <!-- FastClick -->
 <script src="{{asset('/module/AdminLTE/plugins/fastclick/fastclick.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('/module/AdminLTE/dist/js/app.min.js')}}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) 
-<script src="{{asset('/module/AdminLTE/dist/js/pages/dashboard.js')}}"></script>
--->
 <!-- AdminLTE for demo purposes -->
 <script src="{{asset('/module/AdminLTE/dist/js/demo.js')}}"></script>
-<!-- icheck -->
-<script src="{{asset('/module/AdminLTE/plugins/iCheck/icheck.min.js')}}"></script>
 
 </body>
 </html>
